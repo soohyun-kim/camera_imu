f0e4368eeef94ea72738ad6be136d3b043f18874
soohyun.kim <sokim@gatech.edu>
2021-09-05 16:45:06 -0400

    added conversion from intrinsic angles to extrinsic

--- blender_script.py
+++ blender_script.py
@@ -1,6 +1,8 @@
 import bpy, mathutils, math, csv, os
 
 os.chdir('/camera_imu/results')
+if len(os.listdir())==0:
+    print("No files!")
 for fileinc in range(len(os.listdir())):
     dataReader = csv.reader(open(str(fileinc)+'.csv'))
 
@@ -11,7 +13,7 @@ for fileinc in range(len(os.listdir())):
     for frame in dataReader:
         angleList = []
         for value in frame:
-            angleList.append(float(value))
+            angleList.insert(0, float(value))
         bpy.context.scene.frame_set(i)
         bpy.data.objects['cameraController'+str(fileinc)].rotation_euler = mathutils.Euler(angleList)
         bpy.data.objects['cameraController'+str(fileinc)].keyframe_insert(data_path='rotation_euler')
